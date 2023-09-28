7aa3ab9da5073bbb0f0a1c6bfa1d408693d1a0a0
Filip <filip.fieduk@gmail.com>
2023-09-28 15:50:11 +0200

    change seed.py

--- app.py
+++ app.py
@@ -10,108 +10,106 @@ def create_app():
     app.config['SQLALCHEMY_DATABASE_URI'] = config('DATABASE_URL')
     db.init_app(app)
 
+    @app.route('/api/cities', methods=['GET'])
+    def get_cities():
+        cities = City.query.all()
+        return jsonify([city.to_dict() for city in cities])
+
+    @app.route('/api/city/<int:city_id>/cinemas', methods=['GET'])
+    def get_cinemas(city_id):
+        city = City.query.get(city_id)
+        if city is None:
+            abort(404, "Podane miasto nie istnieje")
+        cinemas = city.cinemas
+        return jsonify([cinema.to_dict() for cinema in cinemas])
+
+    @app.route('/api/cinema/<int:cinema_id>', methods=['GET'])
+    def get_cinema(cinema_id):
+        cinema = Cinema.query.get(cinema_id)
+        if cinema is None:
+            abort(404, "Podane kino nie istnieje")
+        return jsonify(cinema.to_dict())
+
+    @app.route('/api/cinema/<int:cinema_id>/showtimes', methods=['GET'])
+    def get_showtimes(cinema_id):
+        cinema = Cinema.query.get(cinema_id)
+        if cinema is None:
+            abort(404, "Podane kino nie istnieje")
+        all_showtimes = []
+        for hall in cinema.halls:
+            all_showtimes.extend(hall.showtimes)
+
+        return jsonify([showtime.to_dict() for showtime in all_showtimes])
+
+    @app.route('/api/showtimes/<int:showtime_id>', methods=['GET'])
+    def get_showtime(showtime_id):
+        showtime = Showtime.query.get(showtime_id)
+        if showtime is None:
+            abort(404, "Złe Id dla seansu")
+        return jsonify(showtime.to_dict())
+
+    @app.route('/api/movie/<int:movie_id>', methods=['GET'])
+    def get_movie(movie_id):
+        movie = Movie.query.get(movie_id)
+        if movie is None:
+            abort(404, "Podany film nie istnieje")
+        return jsonify(movie.to_dict())
+
+    @app.route('/api/select_seats/<int:showtime_id>', methods=['GET'])
+    def get_seats(showtime_id):
+        showtime = Showtime.query.get(showtime_id)
+        if showtime is None:
+            return abort(404, "Złe Id dla seansu")
+
+        taken_seats = [seat.to_dict() for seat in showtime.get_taken_seats()]
+        all_seats = [seat.to_dict() for seat in showtime.hall.seats]
+
+        return jsonify({
+            "taken_seats": taken_seats,
+            "all_seats": all_seats
+        })
+
+    @app.route('/api/ticket_reservation/<int:showtime_id>/confirm_reservation', methods=['POST'])
+    def confirm_reservation(showtime_id):
+        client_name = request.json.get("client_name")
+        seat_ids = request.json.get("seat_ids")
+
+        reservation_uuid = str(uuid.uuid4())
+        reservation = ReservationTicket(client_name=client_name, showtime_id=showtime_id, seat_ids=seat_ids,
+                                        uuid=reservation_uuid)
+        db.session.add(reservation)
+        db.session.commit()
+
+        return jsonify({"ticket_reservation_id": reservation.id})
+
+    @app.route('/api/reservation_details/<int:reservation_ticket_id>', methods=['GET'])
+    def get_reservation_details(reservation_ticket_id):
+        reservation = ReservationTicket.query.filter_by(id=reservation_ticket_id).first()
+        if not reservation:
+            return jsonify({"error": "Rezerwacja nie została znaleziona"}), 404
+
+    @app.route('/api/repertuar', methods=['GET'])
+    def get_repertuar():
+        today = datetime.now().date()
+        now_playing_showtimes = Showtime.query.filter(Showtime.date >= today).all()
+
+        unique_movies = set()
+
+        for showtime in now_playing_showtimes:
+            unique_movies.add(showtime.movie)
+
+        movies_list = [movie.to_dict() for movie in unique_movies]
+
+        return jsonify(movies_list)
+
     return app
 
 
+app = create_app()
 
 
-@app.route('/api/cities', methods=['GET'])
-def get_cities():
-    cities = City.query.all()
-    return jsonify([city.to_dict() for city in cities])
+if __name__ == "__main__":
+    app.run()
 
 
-@app.route('/api/city/<int:city_id>/cinemas', methods=['GET'])
-def get_cinemas(city_id):
-    city = City.query.get(city_id)
-    if city is None:
-        abort(404, "Podane miasto nie istnieje")
-    cinemas = city.cinemas
-    return jsonify([cinema.to_dict() for cinema in cinemas])
-
-
-@app.route('/api/cinema/<int:cinema_id>', methods=['GET'])
-def get_cinema(cinema_id):
-    cinema = Cinema.query.get(cinema_id)
-    if cinema is None:
-        abort(404, "Podane kino nie istnieje")
-    return jsonify(cinema.to_dict())
-
-
-@app.route('/api/cinema/<int:cinema_id>/showtimes', methods=['GET'])
-def get_showtimes(cinema_id):
-    cinema = Cinema.query.get(cinema_id)
-    if cinema is None:
-        abort(404, "Podane kino nie istnieje")
-    all_showtimes = []
-    for hall in cinema.halls:
-        all_showtimes.extend(hall.showtimes)
-
-    return jsonify([showtime.to_dict() for showtime in all_showtimes])
-
-
-@app.route('/api/showtimes/<int:showtime_id>', methods=['GET'])
-def get_showtime(showtime_id):
-    showtime = Showtime.query.get(showtime_id)
-    if showtime is None:
-        abort(404, "Złe Id dla seansu")
-    return jsonify(showtime.to_dict())
-
-
-@app.route('/api/movie/<int:movie_id>', methods=['GET'])
-def get_movie(movie_id):
-    movie = Movie.query.get(movie_id)
-    if movie is None:
-        abort(404, "Podany film nie istnieje")
-    return jsonify(movie.to_dict())
-
-
-@app.route('/api/select_seats/<int:showtime_id>', methods=['GET'])
-def get_seats(showtime_id):
-    showtime = Showtime.query.get(showtime_id)
-    if showtime is None:
-        return abort(404, "Złe Id dla seansu")
-
-    taken_seats = [seat.to_dict() for seat in showtime.get_taken_seats()]
-    all_seats = [seat.to_dict() for seat in showtime.hall.seats]
-
-    return jsonify({
-        "taken_seats": taken_seats,
-        "all_seats": all_seats
-    })
-
-
-@app.route('/api/ticket_reservation/<int:showtime_id>/confirm_reservation', methods=['POST'])
-def confirm_reservation(showtime_id):
-    client_name = request.json.get("client_name")
-    seat_ids = request.json.get("seat_ids")
-
-    reservation_uuid = str(uuid.uuid4())
-    reservation = ReservationTicket(client_name=client_name, showtime_id=showtime_id, seat_ids=seat_ids, uuid=reservation_uuid)
-    db.session.add(reservation)
-    db.session.commit()
-
-    return jsonify({"ticket_reservation_id": reservation.id})
-
-
-@app.route('/api/reservation_details/<int:reservation_ticket_id>', methods=['GET'])
-def get_reservation_details(reservation_ticket_id):
-    reservation = ReservationTicket.query.filter_by(id=reservation_ticket_id).first()
-    if not reservation:
-        return jsonify({"error": "Rezerwacja nie została znaleziona"}), 404
-
-
-@app.route('/api/repertuar', methods=['GET'])
-def get_repertuar():
-    today = datetime.now().date()
-    now_playing_showtimes = Showtime.query.filter(Showtime.date >= today).all()
-
-    unique_movies = set()
-
-    for showtime in now_playing_showtimes:
-        unique_movies.add(showtime.movie)
-
-    movies_list = [movie.to_dict() for movie in unique_movies]
-
-    return jsonify(movies_list)
 
--- seed.py
+++ seed.py
@@ -1,15 +1,15 @@
 import datetime
-from app import create_app, db
+from app import create_app
 from models import db, Showtime, Hall, Movie, Cinema, City
 from tmdb_api import set_global_genres, fetch_popular_movie, save_movie_if_not_exist
 
 app = create_app()
 
 
-def init_db(app_instance, db_instance):
-    with app_instance.app_context():
-        db_instance.drop_all()
-        db_instance.create_all()
+def init_db():
+    with create_app().app_context():
+        db.drop_all()
+        db.create_all()
 
         set_global_genres()
         popular_movies = fetch_popular_movie()
@@ -18,17 +18,17 @@ def init_db(app_instance, db_instance):
         cities = ["Gdańsk", "Gdynia", "Sopot"]
         for city_name in cities:
             city_obj = City(name=city_name)
-            db_instance.session.add(city_obj)
+            db.session.add(city_obj)
 
             for i in range(2):
                 cinema = Cinema(name=f"Kino {city_name} {i+1}", city=city_obj)
-                db_instance.session.add(cinema)
+                db.session.add(cinema)
 
                 for j in range(4):
                     hall = Hall(name=f"Sala {j+1}", rows=10, columns=10, cinema=cinema)
-                    db_instance.session.add(hall)
+                    db.session.add(hall)
 
-        db_instance.session.commit()
+        db.session.commit()
 
         popular_movie_ids = [movie['id'] for movie in popular_movies]
         movies_to_show = Movie.query.filter(Movie.tmdb_id.in_(popular_movie_ids)).all()
@@ -45,14 +45,12 @@ def init_db(app_instance, db_instance):
                         start_time = datetime.datetime.combine(today + datetime.timedelta(days=day),
                                                                datetime.time(hour=hour))
                         showtime = Showtime(start_time=start_time, date=start_time.date(), movie=movie, hall=hall)
-                        db_instance.session.add(showtime)
+                        db.session.add(showtime)
 
                         movie_index = (movie_index + 1) % len(movies_to_show)
 
-        db_instance.session.commit()
+        db.session.commit()
 
 
 if __name__ == "__main__":
-    with app.app_context():
-        if not City.query.first():
-            init_db(app, db)
+    init_db()
